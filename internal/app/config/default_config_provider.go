eac474296b1a8ee58f184fd632e788285937c109
Sviatoslav Tregubov <tregubov.sv@yandex.ru>
2025-10-12 17:41:18 +0300

    fix iter2 test

--- internal/app/config/default_config_provider.go
+++ internal/app/config/default_config_provider.go
@@ -10,7 +10,7 @@ type DefaultProvider struct{}
 func (d *DefaultProvider) setValues(c *Config) error {
 	c.Host = "localhost:8080"
 	c.ShortURLHost = "http://localhost:8080"
-	c.FileStoragePath = ""
+	c.FileStoragePath = "store"
 	c.DatabaseDSN = ""
 	c.AuthSecret = d.getAuthSecret()
 	c.AuditFile = "audit.log"
